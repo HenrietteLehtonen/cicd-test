15d1c814039984a60491cca542360197fac7b058
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2025-09-08 10:27:41 +0300

    update

--- db/create-db.sql
+++ db/create-db.sql
@@ -1,8 +1,8 @@
 DROP DATABASE IF EXISTS cicdtest;
 CREATE DATABASE cicdtest;
-CREATE USER 'myusername'@'localhost' IDENTIFIED BY 'mypassword';
-GRANT ALL PRIVILEGES ON `cicdtest`.* TO 'myusername'@'localhost';
-FLUSH PRIVILEGES;
+-- CREATE USER 'myusername'@'localhost' IDENTIFIED BY 'mypassword';
+-- GRANT ALL PRIVILEGES ON `cicdtest`.* TO 'myusername'@'localhost';
+-- FLUSH PRIVILEGES;
 
 USE cicdtest;
 
